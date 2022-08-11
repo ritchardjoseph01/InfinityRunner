a5f1b903e781b836c0e037f02d88a0aad2725cf5
ritchardjoseph01 <ritchardjoseph01@gmail.com>
2022-08-11 01:42:22 +0100

    Camera
    Camera follows player and stays in the middle

--- InfinityRunner/Assets/Scripts/PlayerMovement.cs
+++ InfinityRunner/Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,12 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody rb;
 
     float horizontalInput;
+    public float horizontalMultiplier = 2;
 
     private void FixedUpdate()
     {
         Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
-        Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.fixedDeltaTime;
+        Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.fixedDeltaTime * horizontalMultiplier;
         rb.MovePosition(rb.position + forwardMove + horizontalMove);
     }
 
