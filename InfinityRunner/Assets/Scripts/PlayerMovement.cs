c5a5fe9c45d7bd14a4f0bd57718a2a853fd77ab0
ritchardjoseph01 <ritchardjoseph01@gmail.com>
2022-08-11 01:18:23 +0100

    Player Movement
    Player now moves forward

--- InfinityRunner/Assets/Scripts/PlayerMovement.cs
+++ InfinityRunner/Assets/Scripts/PlayerMovement.cs
@@ -3,10 +3,16 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5;
+    public Rigidbody rb;
 
+    private void FixedUpdate()
+    {
+        Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + forwardMove);
+    }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
     }
