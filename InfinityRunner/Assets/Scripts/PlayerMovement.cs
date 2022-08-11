62e97e544e31329fc72be331f86f8c698889596a
ritchardjoseph01 <ritchardjoseph01@gmail.com>
2022-08-11 01:34:07 +0100

    Update PlayerMovement.cs
    Player can go left and right

--- InfinityRunner/Assets/Scripts/PlayerMovement.cs
+++ InfinityRunner/Assets/Scripts/PlayerMovement.cs
@@ -5,15 +5,18 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 5;
     public Rigidbody rb;
 
+    float horizontalInput;
+
     private void FixedUpdate()
     {
         Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
-        rb.MovePosition(rb.position + forwardMove);
+        Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + forwardMove + horizontalMove);
     }
 
     // Update is called once per frame
     private void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
     }
 }
