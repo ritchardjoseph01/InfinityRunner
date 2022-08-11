2d5a8668e70a71879e86b1a52cd18332ebbdef31
ritchardjoseph01 <ritchardjoseph01@gmail.com>
2022-08-11 01:11:00 +0100

    Start
    Added Player, Floor and Player Script

---
+++ InfinityRunner/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
